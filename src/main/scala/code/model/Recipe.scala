64810db69c4353e85d7d24f525f85f09e03481bb
Adam Hinz <hinz.adam@gmail.com>
2011-04-24 17:16:07 -0400

    added recipe model

--- src/main/scala/code/model/Recipe.scala
+++ src/main/scala/code/model/Recipe.scala
@@ -6,10 +6,22 @@ import net.liftweb.util._
 import net.liftweb.common._
 
 /**
- *aa
+ * A Recipe is simply an online link to a given recipe with a name, rating, and description
  */
 class Recipe extends LongKeyedMapper[Recipe] with IdPK {
-
+  def getSingleton = Recipe
+  
+  object name extends MappedString(this, 200)
+  object url extends MappedString(this, 3000)
+  object rating extends MappedInt(this)
  
+  object desc extends MappedTextarea(this, 2048) {
+    override def textareaRows  = 10
+    override def textareaCols = 50
+    override def displayName = "Personal Essay"
+  }
+}
 
+object Recipe extends Recipe with LongKeyedMetaMapper[Recipe]  {
+  override def fieldOrder = List(name, url, rating)
 }
