f1fd2242ebd7a3c629d6f2bafec82b295e289c89
Adam Hinz <hinz.adam@gmail.com>
2011-04-24 16:55:11 -0400

    initial lift app

---
+++ src/main/scala/code/model/Recipe.scala
@@ -0,0 +1,15 @@
+package code
+package model
+
+import net.liftweb.mapper._
+import net.liftweb.util._
+import net.liftweb.common._
+
+/**
+ *aa
+ */
+class Recipe extends LongKeyedMapper[Recipe] with IdPK {
+
+ 
+
+}
